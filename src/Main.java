e03c72cb706c9bc2e8116bbab5e4447f704dba96
Stas <www.stas011@mail.ru>
2022-10-04 12:08:09 +0300

    added removeIf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -7,9 +6,11 @@ public class Main {
         List<Person> list = new ArrayList<>();
         list.add(new Person("Dima", "Pupkovich", 25));
         list.add(new Person("Sasha", "Dedovich", 76));
-        list.add(new Person("Olya", "Babushkina", 30));
+        list.add(new Person("Olya", "Babushkina", 12));
         list.add(new Person("Stas", "Pletnev", 21));
         list.add(new Person("Dany", "Akrestl", 15));
+
+        list.removeIf(person -> person.getAge() < 18);
 
         list.sort(new PersonsComparator(8));
         System.out.println(list);
