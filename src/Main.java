9ca63708881c838afa3618889f318b2aafc2059f
Stas <www.stas011@mail.ru>
2022-10-04 11:28:20 +0300

    Add lambda

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
+        //int minChar = 8;
         List<Person> list = new ArrayList<>();
         list.add(new Person("Dima", "Pupkovich", 25));
         list.add(new Person("Sasha", "Dedovich", 76));
@@ -11,7 +14,18 @@ public class Main {
         list.add(new Person("Stas", "Pletnev", 21));
         list.add(new Person("Dany", "Akrestl", 15));
 
-        list.sort(new PersonsComparator(8));
+        list.sort((Person o1, Person o2) -> {
+            if (o1.getSurname().length() > 8 && o2.getSurname().length() > 8) {
+                return Integer.compare(o2.getAge(), o1.getAge());
+            }
+            int num = Integer.compare(o2.getSurname().length(), o1.getSurname().length());
+            if (num == 1 || num == -1) {
+                return num;
+            } else {
+                return Integer.compare(o2.getAge(), o1.getAge());
+            }
+        });
+
         System.out.println(list);
     }
 }
